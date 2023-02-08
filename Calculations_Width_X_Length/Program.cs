51e851ac3e1918f686843393826c0b515306237e
B-ANTONY-J <64221875+B-ANTONY-J@users.noreply.github.com>
2023-02-08 00:07:59 -0500

    added "\n" to provide space within the code instead of using "Console.Writeline()

--- Calculations_Width_X_Length/Program.cs
+++ Calculations_Width_X_Length/Program.cs
@@ -2,15 +2,13 @@
 {
     internal class Program
     {       //Const values moved outside of Function to add more stability to the code and to define the values here so they can reused if NEC
-           const int sqFootByHours = 20;
-           const double flooringPerHour = 86.00;
+        const int sqFootByHours = 20;
+        const double flooringPerHour = 86.00;
         static void Main(string[] args)
         {
-          
-            Console.WriteLine();
-                        
-            Console.WriteLine("Hello, Florian & Michael!!!");
-            
+
+            Console.WriteLine($"\nHello, Florian & Michael!!!");
+
             Console.WriteLine();
             Console.WriteLine("Please enter a value For both width and length(after each value press enter).");
             double widthCalc = Convert.ToInt32(Console.ReadLine());
@@ -22,15 +20,15 @@
 
             Console.WriteLine($"\nPlease Enter the Cost per Unit per Square Feet.\n");
             double areaCostPerUnit = Convert.ToDouble(Console.ReadLine());
-            
+
             double areaCost = (widthCalc * areaCostPerUnit) + (lengthCalc * areaCostPerUnit);
-            Console.WriteLine($"The Total cost for units per square footage is " + "$" + areaCost + " dollars.\n" );
+            Console.WriteLine($"The Total cost for units per square footage is " + "$" + areaCost + " dollars.\n");
 
             Console.WriteLine("You can divide sq footage by hours here to find out your labor cost. Enter your total sq footage below.");
             int sqFootTotal = Convert.ToInt32(Console.ReadLine());
             int sqFootageByHours = sqFootTotal / sqFootByHours;
             double sqFootageHrResult = (double)sqFootTotal / (double)sqFootByHours;
-           
+
             double flooringPerHourCalc = flooringPerHour * sqFootageHrResult;
             double laborCostMerge = areaCost;
             double laborAndAreaCalc = (double)(flooringPerHourCalc) + laborCostMerge;
@@ -44,11 +42,11 @@
             Console.WriteLine("Please enter your estinated labor hours below to confirm the calculated flooring work(Cost of Flooring Per Hr is $86.00) is accurate for your request.");
             Console.WriteLine();
             double inputFlooringPerHour = Convert.ToDouble(Console.ReadLine());
-            double flooringPerHourTotal = inputFlooringPerHour* flooringPerHour;
+            double flooringPerHourTotal = inputFlooringPerHour * flooringPerHour;
             //The code below is used to verify the accuracy of the code through out this function
             Console.WriteLine("You have requested " + inputFlooringPerHour + " hours of Labor " + "for a total cost of $ " + flooringPerHourTotal + " dollars");
             Console.WriteLine();
-            
+
 
 
 
